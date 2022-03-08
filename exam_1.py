d8a32786c69f34927258212846af243ab3c6bff0
jinsonjacob2000 <94525804+jinsonjacob2000@users.noreply.github.com>
2022-03-08 12:40:00 +0530

    Add files via upload

---
+++ exam_1.py
@@ -0,0 +1,23 @@
+"'to find the area of triangle'"
+
+length=float(input("enter the length of the triangle"))
+breadth=float(input("enter the breadth of the triangle"))
+
+area=1/2*length*breadth
+print("area of the triangle",area)
+
+"'to find the area of the circle'"
+r=float(input("enter the radius of the circle"))
+
+area1=3.14*r*r
+
+print("the area of the circle is",area1)
+
+"'to find the area of the rectangle'"
+
+l=float(input("enter the length of the rectangle"))
+b=float(input("enter the breadth of the rectangle"))
+
+area2=l*b
+
+print("the area of the rectangle",area2)
